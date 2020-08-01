8235af5c2c3b90157a2590b195e58a5591eceeab
mrbruins <1969831+mrbruins@users.noreply.github.com>
2020-08-01 21:56:18 +0200

    Updated dock configuration, ohmyzsh config, vim and zsh configs.

--- workspaces/shared/run/after/dock.sh
+++ workspaces/shared/run/after/dock.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Finder.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "/System/Applications/System Preferences.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Fantastical.app"
 dockutil --no-restart --add "/Applications/Todoist.app"
-dockutil --no-restart --add "/Applications/Music.app"
+dockutil --no-restart --add "/System/Applications/Music.app/"
 dockutil --no-restart --add "/Applications/Notion.app"
 
 
